f6b507abf933abeadd79a0c53f4462ec91b45fb1
JackFille <jfillingham85@gmail.com>
2017-03-13 00:40:03 +0000

    uploadfix1

---
+++ stuff/index.php
@@ -0,0 +1,26 @@
+<html>
+    
+    <head>
+        
+        <style type="text/css">
+            
+            #save_feedback {
+                display: none;
+            }
+            
+        
+        </style>
+        
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jfunc.js"></script>
+        
+    </head>
+    
+    <body>
+        
+        <input type="button" value="Save" onclick="save();" />
+        <div id="save_feedback"></div>
+        
+    </body>
+
+</html>
